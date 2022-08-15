de0877e0e456481f392eefcdd99ab233b583faf9
Natalya Lobanova <knopka40a@mail.ru>
2022-08-15 19:10:13 +0500

    Add Ex010

---
+++ Ex010/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+int number = ReadInt("Введите трёхзначное число: ");
+int secondDigit = number % 100 / 10;
+Console.WriteLine(secondDigit);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
