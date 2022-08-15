53f5111d9114e8ceb54697d4030adc7c3d36ed79
Natalya Lobanova <knopka40a@mail.ru>
2022-08-15 19:10:58 +0500

    Add Ex013

---
+++ Ex013/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+int number = ReadInt("Введите число: ");
+
+if (number > 999)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }  
+    Console.WriteLine("Третья цифра " + number % 10);  
+}
+
+else if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    Console.WriteLine("Третья цифра " + number % 10);
+}  
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
